a37fbe58750897bd5c05cfb389e75cfaa9a46af5
Art_J <a0985710306@gmail.com>
2023-02-15 23:40:20 +0800

    rwd

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,18 +1,27 @@
+var flag = true;
 $(function() {
     $(".navbar-toggler").on("click", function(e) {
+        var arrow = document.getElementById("arrow");
+        if (flag) {
+            arrow.style.transform = 'scaleX(-1)';
+            flag = false;
+        } else {
+            arrow.style.transform = 'scaleX(1)';
+            flag = true;
+        }
         $(".tm-header").toggleClass("show");
         e.stopPropagation();
-      });
-    
-      $("html").click(function(e) {
+    });
+
+    $("html").click(function(e) {
         var header = document.getElementById("tm-header");
-    
+
         if (!header.contains(e.target)) {
-          $(".tm-header").removeClass("show");
+            $(".tm-header").removeClass("show");
         }
-      });
-    
-      $("#tm-nav .nav-link").click(function(e) {
+    });
+
+    $("#tm-nav .nav-link").click(function(e) {
         $(".tm-header").removeClass("show");
-      });
+    });
 });
